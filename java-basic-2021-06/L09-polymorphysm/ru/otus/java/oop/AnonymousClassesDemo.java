216051455b0309b682e5877383d60249952dc8fe
spv <pvstrekalov@yandex.ru>
2021-08-04 20:45:34 +0400

    L09-polymorphysm

---
+++ java-basic-2021-06/L09-polymorphysm/ru/otus/java/oop/AnonymousClassesDemo.java
@@ -0,0 +1,43 @@
+package ru.otus.java.oop;
+
+public class AnonymousClassesDemo {
+
+    public static void main(String[] args) {
+        Animal monkey = new Animal() {
+            @Override
+            public void speak() {
+                System.out.println("Я обезьяна");
+            }
+        };
+
+        monkey.speak();
+        System.out.println(monkey);
+
+        // для сравнения - класс Cat
+        System.out.println(new Cat());
+
+        // Создадим класс сразу при вызове метода
+        doSomething(new Guarding() {
+            @Override
+            public void guard() {
+                System.out.println("Я анонимный класс");
+            }
+        });
+
+        // можно поля определять
+        Animal fish = new Animal() {
+            private int weight = 10;
+
+            @Override
+            public void speak() {
+                System.out.println("Рыба, вес = " + this.weight);
+            }
+        };
+        fish.speak();
+    }
+
+    private static void doSomething(Guarding guarding){
+        guarding.guard();
+    }
+
+}
