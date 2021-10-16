5b59e0185a4da793fc9cf9b78ee04ab8f64debe5
spv <pvstrekalov@yandex.ru>
2021-10-16 10:39:37 +0400

    2021-09 L05-primitives

---
+++ java-basic-2021-09/L05-primitives/ru/otus/java/enums/Level.java
@@ -0,0 +1,21 @@
+package ru.otus.java.enums;
+
+// https://java-blog.ru/osnovy/java-enum-perechisleniya-s-primerami
+
+public enum Level {
+    HIGH(3),  //calls constructor with value 3
+    MEDIUM(2),  //calls constructor with value 2
+    LOW (1)   //calls constructor with value 1
+    ; // semicolon needed when fields / methods follow
+
+    private final int levelCode;
+
+    Level(int levelCode) {
+        this.levelCode = levelCode;
+    }
+
+    public int getLevelCode() {
+        return this.levelCode;
+    }
+
+}
