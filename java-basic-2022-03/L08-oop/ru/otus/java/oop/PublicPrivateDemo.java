d66083c5f900b7ca91e1d49da776734936ed61f7
spv <pvstrekalov@yandex.ru>
2022-04-27 20:46:09 +0400

    L08-oop

---
+++ java-basic-2022-03/L08-oop/ru/otus/java/oop/PublicPrivateDemo.java
@@ -0,0 +1,11 @@
+package ru.otus.java.oop;
+
+public class PublicPrivateDemo {
+    void doSomething() {
+        Company company = new Company();
+//        company.namePrivate = "";
+        company.nameProtected = "";
+        company.namePublic = "";
+        company.nameDefault = "";
+    }
+}
