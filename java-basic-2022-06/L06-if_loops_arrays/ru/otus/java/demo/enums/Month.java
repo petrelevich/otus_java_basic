bfa48490b90fdf6ae6b184c4c1067e9ff447d8ca
spv <pvstrekalov@yandex.ru>
2022-07-20 20:25:11 +0400

    L06-if_loops_arrays 2022-06

---
+++ java-basic-2022-06/L06-if_loops_arrays/ru/otus/java/demo/enums/Month.java
@@ -0,0 +1,16 @@
+package ru.otus.java.demo.enums;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
